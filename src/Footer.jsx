c672d5eba182b695d72b6697154f05b84ca706d6
Ilknur Mustafov <ilknurilko@gmail.com>
2020-03-29 00:55:38 +0100

    Keeper App Part 1

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+let curentYear = new Date().getFullYear();
+
+function Footer(){
+    return <footer>
+    <p>Copyright {curentYear}</p>
+
+    </footer>;
+}
+export default Footer;
